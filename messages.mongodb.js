6e00758331b583df15617cb64561b53e5c35a202
Solomiia Kushniryk <kushniryksolomiya@gmail.com>
2023-10-24 14:43:39 +0300

    Add count to messages colection mongodb

--- messages.mongodb.js
+++ messages.mongodb.js
@@ -1,3 +1,32 @@
-use('fd_mongo');
+use('messages')
 
-db.messages.insertOne({text:'Hello'})
+// db.messages.insertMany([
+//   { message: 'Паровоз, паровоз там' },
+//   { message: 'Хто?' },
+//   { message: 'Бачу паровоз та інший паровоз' },
+//   { message: '' },
+//   { message: 'бла, бла, бла' }
+// ])
+
+db.messages.aggregate([
+  { $unwind: '$message' },
+  {
+    $match: {
+      message: { $regex: 'паровоз', $options: 'i' }
+    }
+  },
+  { $group: { _id: '$message', count: { $sum: +1 } } },
+  { $project: { count: '$count' } }
+])
+
+// db.messages.aggregate([
+//   { $unwind: '$message' },
+//   {
+//     $match: {
+//       message: { $regex: 'паровоз', $options: 'i' }
+//     }
+//   },
+//   {$count: "count_words"}
+// ])
+
+// db.messages.drop()
